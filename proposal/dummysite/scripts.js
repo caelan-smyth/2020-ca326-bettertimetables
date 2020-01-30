3a13311cec4ab1d48761154d44dca024957cfeab
Caelan <infamouspot@gmail.com>
2020-01-30 13:43:37 +0000

    Removed duplicate files added by mistake

--- proposal/dummysite/scripts.js
+++
@@ -1,7 +0,0 @@
-function validateForm() {
-    var x = document.forms["courseform"]["coursetitle"].value;
-    if(x == "") {
-        alert("Please input course name");
-        return false;
-    }
-}
