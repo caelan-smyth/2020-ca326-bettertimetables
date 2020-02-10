434fbff1aeb5953af6149538c1809ae18ff2b7e9
Caelan Smyth <infamouspot@gmail.com>
2020-02-10 14:52:25 +0000

    Updated CSS file, HTML file and created React Search component

--- bettertimetable-app/src/index.js
+++ bettertimetable-app/src/index.js
@@ -1,3 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
+class Search extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {value: ''}
+
+        this.changeHandler = this.changeHandler.bind(this);
+        //this.submitHandler = this.submitHandler.bind(this);
+    }
+
+    changeHandler(event) {
+        let name = event.target.name;
+        let value = event.target.value;
+        this.setState({[name]: value});
+    }
+
+    //submitHandler(event) {}
+
+    render() {
+        return (
+            <form method="get">
+                <label>
+                    Programme:
+                    <input type="text" name="course" onChange={this.changeHandler} />
+                </label><br />
+                <label>
+                    Year of Study:
+                    <input type="text" name="year" onChange={this.changeHandler} />
+                </label><br />
+                <input type="submit" value="Search" />
+            </form>
+        );
+    }
+}
+
+ReactDOM.render(<Search />, document.getElementById('root'));
