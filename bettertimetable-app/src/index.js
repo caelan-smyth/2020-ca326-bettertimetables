9e72c6cf404c8f432f9296620dffd6d31c937c5d
Caelan Smyth <infamouspot@gmail.com>
2020-02-21 10:25:08 +0000

    Added ethical approval form. Modified index.js and index.html

--- bettertimetable-app/src/index.js
+++ bettertimetable-app/src/index.js
@@ -1,65 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './index.css';
+import 'timetablecomponents/case21.js';
+import Search from './search.js';
 
-
-class Search extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            course: '',
-            year: null,
-            semester: null
-        }
-
-        this.changeHandler = this.changeHandler.bind(this);
-        this.submitHandler = this.submitHandler.bind(this);
-    }
-
-    changeHandler(event) {
-        let name = event.target.name;
-        let value = event.target.value;
-        this.setState({[name]: value});
-    }
-
-    submitHandler(event) {
-        event.preventDefault();
-        
-    }
-
+class App extends React.Component {
     render() {
         return (
-            <>
-                <div>
-                    <form method="get">
-                        <label>
-                            Programme:
-                            <input type="text" name="course" onChange={this.changeHandler} />
-                        </label>
-                        <label>
-                            Year of Study:
-                            <input type="text" name="year" onChange={this.changeHandler} />
-                        </label>
-                        <label>
-                            Semester:
-                            <select name="semester" >
-                                <option value="one">1</option>
-                                <option value="two">2</option>
-                            </select>
-                        </label>
-                        <label>
-                            Search
-                            <input type="Submit" value="Submit" name="search for course" />
-                        </label>
-                    </form>
-                </div>
-                <BrowserRouter>
-                    <Route path ="/" exact component={Search} />
-                </BrowserRouter>
-            </>
+            <Router>
+                <Switch>
+                    <Route path="/" exact component={Search} />
+                </Switch>
+            </Router>
         );
-    }
+    }   
 }
 
-ReactDOM.render(<Search />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
