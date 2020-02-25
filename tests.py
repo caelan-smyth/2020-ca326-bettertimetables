b9f841646dafd5182f1f8223c89027bb7b4d4046
Oisin <oisinhenry@gmail.com>
2020-02-25 18:18:28 +0000

    added CI/CD yaml and created tests.py

---
+++ tests.py
@@ -0,0 +1,6 @@
+import unittest
+
+class TestTesting(unittest.TestCase):
+
+    def test_test(self): # basic test to proc CI/CD and make sure all is working
+        self.assertEqual('foo'.upper(), 'FOO')
