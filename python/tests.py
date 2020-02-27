81fbf1657ed48c3d649aaa5e8e8fdc08c9454246
Oisin <oisinhenry@gmail.com>
2020-02-27 17:39:30 +0000

    added linter in CI for js files

--- python/tests.py
+++ python/tests.py
@@ -4,3 +4,6 @@ class TestTesting(unittest.TestCase):
 
     def test_test(self): # basic test to proc CI/CD and make sure all is working
         self.assertEqual('foo'.upper(), 'FOO')
+
+if __name__ == "__main__":
+    unittest.main()
